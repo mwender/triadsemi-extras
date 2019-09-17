b4f1e7c17b88cc95e567090c27e062dafb2b3278
Michael Wender <michael@michaelwender.com>
2019-09-17 14:12:27 -0400

    First commit

---
+++ acf-advanced-shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     ACF Advanced Shortcode
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Handles shortcode output for advanced ACF field groups.
+ * Author:          TheWebist
+ * Author URI:      https://mwender.com
+ * Text Domain:     acf-advanced-shortcode
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Acf_Advanced_Shortcode
+ */
+
+// Your code starts here.
