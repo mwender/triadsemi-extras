f991b753bd53d16cfc862d895b5b629eb683007a
Michael Wender <michael@michaelwender.com>
2020-03-26 12:00:20 -0400

    Documenting 1.0.1

--- triadsemi-extras.php
+++ triadsemi-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       triadsemi-extras
  * Domain Path:       /languages
- * Version:           1.0.0
+ * Version:           1.0.1
  *
  * @package           Triadsemi_Extras
  */
