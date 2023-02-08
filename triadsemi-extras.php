8f37ea5f04836cd91bae9f2a6afb908b3d945012
Michael Wender <michael@michaelwender.com>
2023-02-08 11:44:34 -0500

    Updating Performance Technical Sales reps

--- triadsemi-extras.php
+++ triadsemi-extras.php
@@ -8,7 +8,7 @@
  * Author URI:        https://mwender.com
  * Text Domain:       triadsemi-extras
  * Domain Path:       /languages
- * Version:           1.2.0
+ * Version:           1.3.0
  *
  * @package           Triadsemi_Extras
  */
